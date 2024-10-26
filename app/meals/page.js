ac9328cc8bdb5f73d276713f48ba505af1b3ff2b
shoaibalirts <shoaibali.rts@gmail.com>
2024-10-26 16:56:27 +0200

    used meals route to share meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 export default function Page() {
-  return <p>This is a Meals Route</p>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
