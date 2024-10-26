37b85a0c2e36752184f9c8f37f03d6c79a2dd330
shoaibalirts <shoaibali.rts@gmail.com>
2024-10-26 17:28:56 +0200

    added lib folder and created meals.js file in order to connect and deal with a database

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,7 +2,7 @@ import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
